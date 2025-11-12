4b566388da83c7a20e58f6daa112a304ae0b525e
shanzzz321 <shanleyaninzo@gmail.com>
2025-11-12 16:54:16 -0500

    Fixed error messages

--- src/main/java/org/example/zoomanagementsystem/Controllers/AnimalViewController.java
+++ src/main/java/org/example/zoomanagementsystem/Controllers/AnimalViewController.java
@@ -3,8 +3,6 @@ package org.example.zoomanagementsystem.Controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.example.zoomanagementsystem.Model.*;
@@ -21,12 +19,6 @@ public class AnimalViewController {
 
     @FXML
     private TextField animalAgeText;
-
-    @FXML
-    private Button animalSaveButton;
-
-    @FXML
-    private Button animalCancelButton;
 
     private Animal aResultAnimal;
     private Animal aOriginalAnimal;
@@ -55,7 +47,7 @@ public class AnimalViewController {
         String pAgeStr = animalAgeText.getText().trim();
 
         if (pType.isEmpty() || pName.isEmpty() || pAgeStr.isEmpty()) {
-            System.out.println("Please fill all the fields");
+            showAlert("Please fill all the fields");
             return;
         }
 
@@ -63,12 +55,12 @@ public class AnimalViewController {
         try {
             pAge = Double.parseDouble(pAgeStr);
             if (pAge < 0) {
-                System.out.println("Please enter a valid number");
+                showAlert("Please enter a valid number");
                 return;
             }
         }
         catch (NumberFormatException e) {
-            System.out.println("Please enter a valid number");
+            showAlert("Please enter a valid number");
             return;
         }
 
@@ -83,13 +75,14 @@ public class AnimalViewController {
             newAnimal = new Cougar(pName, pAge);
         }
         else {
-            System.out.println("Please enter a valid animal type");
+            showAlert("Please enter a valid animal type");
             return;
         }
 
         aResultAnimal = newAnimal;
         closeWindow();
     }
+
 
     public void onCancelButtonClick(ActionEvent actionEvent) {
         aResultAnimal = null;
@@ -108,4 +101,11 @@ public class AnimalViewController {
     public Animal getOriginalAnimal() {
         return aOriginalAnimal;
     }
+
+    private void showAlert(String message) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
+
